c171f5aac68d3d09363d053c92db70a036e887b4
Jérémie <jeremjao@gmail.com>
2023-08-30 12:58:24 +0200

    terrain change

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import * as THREE from 'three'
 import { Html, OrbitControls } from "@react-three/drei"
 import { useControls } from 'leva'
+import Terrain from "./Terrain"
 
 export default function Scene() {
     const { position, positionText, color, generalLight, directionalLight } = useControls({
@@ -21,24 +22,26 @@ export default function Scene() {
         },
         color: '#ff0000',
         generalLight: {
-            value: 2,
+            value: -4.3,
             min: - 10,
             max: 10,
             step: 0.01,
         },
         directionalLight: {
-            value: 1.5,
+            value: 6.47,
             min: - 10,
             max: 10,
             step: 0.01,
         }
     })
+
+
     return (<>
         <OrbitControls />
         <ambientLight intensity={generalLight} />
         <directionalLight position={[1, 2, 3]} intensity={directionalLight} />
-        <mesh position={[position.x, position.y, 0]} scale={1}>
-            <torusKnotGeometry />
+        <group position={[position.x, position.y, 0]} scale={1}>
+            <Terrain />
             <Html distanceFactor={10} position={[positionText.x, positionText.y, positionText.z]} className="bg-gunmetal p-5 rounded-lg text-melon w-fit opacity-75
 
 ">
@@ -46,6 +49,7 @@ export default function Scene() {
                 <p>ici on peut raconter l'histoire de la CGN</p>
             </Html>
             <meshToonMaterial color={color} />
-        </mesh>
+        </group>
+
     </>)
 }
