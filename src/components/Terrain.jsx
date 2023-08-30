867fd56972804f50c1dc91cf55b534eb820c59f3
Jérémie <jeremjao@gmail.com>
2023-08-30 11:43:54 +0200

    ajout terrain

--- src/components/Terrain.jsx
+++ src/components/Terrain.jsx
@@ -1,10 +1,12 @@
-
+import { useGLTF } from '@react-three/drei'
+import { MeshToonMaterial } from 'three';
 
 export default function Terrain() {
-
+    const modelTerrain = useGLTF('../src/assets/model/terrain.glb')
 
     return (<>
-
-
+        <mesh scale={10} geometry={modelTerrain.nodes.terrain.geometry}>
+            <meshToonMaterial color={'red'} />
+        </mesh>
     </>)
 }
