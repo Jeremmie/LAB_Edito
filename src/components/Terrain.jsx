9394bb24477ba94c9721d867e544b730ab054d91
Jérémie <jeremjao@gmail.com>
2023-08-30 13:08:41 +0200

    Terrain tweak

--- src/components/Terrain.jsx
+++ src/components/Terrain.jsx
@@ -1,12 +1,17 @@
 import { useGLTF } from '@react-three/drei'
 import { MeshToonMaterial } from 'three';
+import { useControls } from 'leva';
 
 export default function Terrain() {
     const modelTerrain = useGLTF('../src/assets/model/terrain.glb')
 
+    const { color } = useControls({
+        color: '#ff0000',
+    })
+
     return (<>
         <mesh scale={10} geometry={modelTerrain.nodes.terrain.geometry}>
-            <meshToonMaterial color={'red'} />
+            <meshToonMaterial color={color} />
         </mesh>
     </>)
 }
