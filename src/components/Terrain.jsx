a36493b04a9595ce79a59511f9073d796075af52
Jérémie <jeremjao@gmail.com>
2023-08-31 14:02:42 +0200

    add geneve

--- src/components/Terrain.jsx
+++ src/components/Terrain.jsx
@@ -3,14 +3,14 @@ import { MeshToonMaterial } from 'three';
 import { useControls } from 'leva';
 
 export default function Terrain() {
-    const modelTerrain = useGLTF('../src/assets/model/terrain.glb')
+    const modelTerrain = useGLTF('../src/assets/model/geneve.glb')
 
     const { color } = useControls({
         color: '#ff0000',
     })
 
     return (<>
-        <mesh scale={10} geometry={modelTerrain.nodes.terrain.geometry}>
+        <mesh scale={0.1} geometry={modelTerrain.nodes.geneve.geometry}>
             <meshToonMaterial color={color} />
         </mesh>
     </>)
