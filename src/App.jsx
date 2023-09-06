c610fd512bb1d688edf8eeb7acc79a8a4f76709c
Jérémie <jeremjao@gmail.com>
2023-09-06 15:28:43 +0200

    Revert "Revert "recup avant theathre""
    This reverts commit 8b21894b7a9f68de14e09a26d25d57a4835da7d7.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useReducer, useRef, useState } from 'react'
+import { getProject, val } from "@theatre/core";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './index.css'
@@ -6,19 +7,48 @@ import Scene from './components/Scene'
 import { Canvas } from '@react-three/fiber'
 import { useControls } from 'leva'
 
+import {
+  SheetProvider,
+  PerspectiveCamera,
+  useCurrentSheet,
+} from "@theatre/r3f";
+
+
 
 
 function App() {
 
   const { bgColor } = useControls({
-    bgColor: '#6FD2FF',
+    bgColor: '#2b67d1',
   })
+
+  const pageBtn = useRef()
+  const openMenu = () => {
+    var menuVisible = document.querySelector('.slidePage');
+    menuVisible.classList.toggle('popUpBtnOpen');
+  }
+
+  const sheet = getProject("Fly Through").sheet("Scene");
 
   return (
     <>
-      <Canvas className='h-screen'>
+      <div ref={pageBtn} onClick={openMenu} className='w-fit bg-melon px-2 py-1 absolute z-40 rounded-full transitionAll'>open</div>
+      <div className='rounded-t-lg slidePage -bottom-full w-screen fixed z-30 bg-gunmetal/90 p-5 text-white transitionAll'>
+        <h1 className='FONTCONDENSEND mt-5 text-4xl'>débarcadère</h1>
+        <hr />
+        <p className='mt-5'>Le débarcadère de Genève à jouer un role important dans la concurrence entre les deux compagnies.</p>
+        <div className='underline decoration-solid mt-2'>lire plus</div>
+      </div>
+      {/* <div className='popUpBtn popUpBtnOpen bg-transparent flex justify-center align-center'>onclick</div> */}
+
+
+      <Canvas gl={{ preserveDrawingBuffer: true }} className='z-0 fixed h-screen'>
         <color args={[bgColor]} attach="background" />
-        <Scene />
+        <ScrollControls pages={5}>
+          <SheetProvider sheet={sheet}>
+            <Scene />
+          </SheetProvider>
+        </ScrollControls>
       </Canvas>
     </>
   )
