ad44043b95d2243f69697b9e29d8386dc06443f9
Jérémie <jeremjao@gmail.com>
2023-09-07 09:10:04 +0200

    Revert "tweak bgColor"
    This reverts commit 298bd59d82ecb0d4a198baa90bfd484cbf298412.

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './index.css'
 import Scene from './components/Scene'
-import { Canvas, useFrame } from "@react-three/fiber";
-import { Gltf, ScrollControls, useScroll } from "@react-three/drei";
-import { getProject, val } from "@theatre/core";
+import { Canvas } from '@react-three/fiber'
+import { useControls } from 'leva'
 
-import {
-  SheetProvider,
-  PerspectiveCamera,
-  useCurrentSheet,
-} from "@theatre/r3f";
+
 
 function App() {
   const sheet = getProject("Fly Through").sheet("Scene");
@@ -19,12 +14,9 @@ function App() {
 
   return (
     <>
-      <Canvas gl={{ preserveDrawingBuffer: true }} className='h-screen bg-melon'>
-        <ScrollControls pages={5}>
-          <SheetProvider sheet={sheet}>
-            <Scene />
-          </SheetProvider>
-        </ScrollControls>
+      <Canvas className='h-screen'>
+        <color args={[bgColor]} attach="background" />
+        <Scene />
       </Canvas>
     </>
   )
